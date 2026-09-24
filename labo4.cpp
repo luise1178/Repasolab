197ecd0be2babe83ea89e6554bb6d7dc30acbcb6
luismolina <00368826@uca.edu.sv>
2026-09-24 15:24:32 -0600

    agregando lab 4

---
+++ labo4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+int numero;
+cout <<"ingresar numero";
+cin >> numero;
+
+if (numero>0)
+{
+    cout <<"su numero es positivo"<< endl; /* ode */
+}
+else if (numero<0){
+    cout <<"su numero es negativo"<< endl;
+}else {
+    cout <<"su numero es 0"<< endl;
+}
+
+
+
+
+
+
+
+
+
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
